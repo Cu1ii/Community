5806ab4d632f0f18d055f4e2a72b350b15e5c991
Cu1 <cu1universe@163.com>
2021-12-16 22:05:26 +0800

    整合 Kafka

---
+++ src/test/java/com/cu1/community/KafkaTests.java
@@ -0,0 +1,21 @@
+package com.cu1.community;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+@SpringBootTest
+public class KafkaTests {
+
+    @Test
+    public void testKafka() {
+
+    }
+
+}
+
+@Component
+class KafkaProducer {
+
+}
